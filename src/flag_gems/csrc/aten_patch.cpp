16a38c3dbd7e0926759e11412cc183d676c32c2b
kiddyjinjin <kiddyjinjin@gmail.com>
2026-01-02 04:40:17 +0000

    disable not ok cpp wrapper

--- src/flag_gems/csrc/aten_patch.cpp
+++ src/flag_gems/csrc/aten_patch.cpp
@@ -38,12 +38,12 @@ TORCH_LIBRARY_IMPL(aten, CUDA, m) {
   REGISTER_AND_LOG("zeros", zeros);
   REGISTER_AND_LOG("fill.Scalar", fill_scalar);
   REGISTER_AND_LOG("fill_.Scalar", fill_scalar_);
-  REGISTER_AND_LOG("fill.Tensor", fill_tensor);
-  REGISTER_AND_LOG("fill_.Tensor", fill_tensor_);
+  // REGISTER_AND_LOG("fill.Tensor", fill_tensor);
+  // REGISTER_AND_LOG("fill_.Tensor", fill_tensor_);
   // REGISTER_AND_LOG("_to_copy", to_copy);
   // REGISTER_AND_LOG("copy_", copy_);
-  REGISTER_AND_LOG("to.dtype", to_dtype);
-  REGISTER_AND_LOG("to.other", to_other);
+  // REGISTER_AND_LOG("to.dtype", to_dtype);
+  // REGISTER_AND_LOG("to.other", to_other);
   REGISTER_AND_LOG("clone", clone);
 }
 
