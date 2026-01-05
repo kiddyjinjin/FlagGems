aaf623a90cf1faf36b3ec9b4a8cd9e89283af69c
AdvancedCompiler <Pikachu_Jun@outlook.com>
2026-01-05 18:48:44 +0800

    [AdvancedCompiler]Add te.swiglu&dswiglu (#1062)

--- src/flag_gems/fused/__init__.py
+++ src/flag_gems/fused/__init__.py
@@ -21,6 +21,7 @@ from flag_gems.fused.rwkv_ka_fusion import rwkv_ka_fusion
 from flag_gems.fused.rwkv_mm_sparsity import rwkv_mm_sparsity
 from flag_gems.fused.silu_and_mul import silu_and_mul, silu_and_mul_out
 from flag_gems.fused.skip_layernorm import skip_layer_norm
+from flag_gems.fused.swiglu import dswiglu, swiglu
 from flag_gems.fused.topk_softmax import topk_softmax
 from flag_gems.fused.weight_norm import weight_norm
 
@@ -31,6 +32,8 @@ __all__ = [
     "fused_add_rms_norm",
     "silu_and_mul",
     "silu_and_mul_out",
+    "swiglu",
+    "dswiglu",
     "geglu",
     "dgeglu",
     "gelu_and_mul",
