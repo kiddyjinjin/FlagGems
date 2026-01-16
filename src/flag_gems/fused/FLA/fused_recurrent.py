2b4bbf4b283e77e958d8b42d5abb5e459cb16367
kiddyjinjin <kiddyjinjin@gmail.com>
2026-01-16 08:23:33 +0000

    update fused_recurrent_gated_delta_rule_fwd for qwen3-next

--- src/flag_gems/fused/FLA/fused_recurrent.py
+++ src/flag_gems/fused/FLA/fused_recurrent.py
@@ -3,10 +3,7 @@
 # the following copyright notice:
 # Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 # ruff: noqa: E501
-<<<<<<< HEAD
 import logging
-=======
->>>>>>> df000e19 (update fla kernels)
 
 import torch
 import triton
@@ -14,7 +11,6 @@ import triton.language as tl
 
 from flag_gems.fused.FLA.triton_ops_helper import exp
 
-<<<<<<< HEAD
 logger = logging.getLogger(__name__)
 
 
@@ -179,8 +175,167 @@ def fused_recurrent_gated_delta_rule_fwd_sp_for_qwen3_next_kernel(
         p_g += HV
         p_beta += HV * (V if IS_BETA_HEADWISE else 1)
 
-=======
->>>>>>> df000e19 (update fla kernels)
+
+@triton.heuristics(
+    {
+        "USE_INITIAL_STATE": lambda args: args["h0"] is not None,
+        "IS_VARLEN": lambda args: args["cu_seqlens"] is not None,
+        "IS_CONTINUOUS_BATCHING": lambda args: args["ssm_state_indices"] is not None,
+        "IS_SPEC_DECODING": lambda args: args["num_accepted_tokens"] is not None,
+    }
+)
+@triton.jit(do_not_specialize=["N", "T"])
+def fused_recurrent_gated_delta_rule_fwd_sp_for_qwen3_next_kernel(
+    q,  # [1, T, 16/tp, 128]
+    k,  # [1, T, 16/tp, 128]
+    v,  # [1, T, 32/tp, 128]
+    g,  # [1, T, 32/tp]
+    beta,  # [1, T, 32/tp]
+    o,  # 同 v， 是输出 [1, T, 32/tp, 128]
+    h0,  # initial_state  # [4589, 8, 128, 128]
+    ht,  # final_state，and since INPLACE_FINAL_STATE is True, we write to initial_state directly
+    cu_seqlens,  # cu_seqlens = range(0, T)
+    ssm_state_indices,  # zeros tensors of shape [T]
+    num_accepted_tokens,  # None
+    scale,  # 0.08838834764831845
+    N: tl.int64,  # num of sequences   # should be 1
+    T: tl.int64,  # num of tokens      # should be T, a dynamic value
+    stride_q_b: tl.int64,
+    stride_k_b: tl.int64,
+    stride_v_b: tl.int64,
+    stride_q_t: tl.int64,
+    stride_k_t: tl.int64,
+    stride_v_t: tl.int64,
+    stride_q_h: tl.int64,
+    stride_k_h: tl.int64,
+    stride_v_hv: tl.int64,
+    stride_q_k: tl.int64,
+    stride_k_k: tl.int64,
+    stride_v_v: tl.int64,
+    B: tl.constexpr,
+    H: tl.constexpr,
+    HV: tl.constexpr,
+    K: tl.constexpr,
+    V: tl.constexpr,
+    BK: tl.constexpr,
+    BV: tl.constexpr,
+    stride_init_state_token: tl.constexpr,
+    stride_final_state_token: tl.constexpr,
+    stride_indices_seq: tl.constexpr,
+    stride_indices_tok: tl.constexpr,
+    USE_INITIAL_STATE: tl.constexpr,  # whether to use initial state
+    INPLACE_FINAL_STATE: tl.constexpr,  # whether to store final state inplace
+    IS_BETA_HEADWISE: tl.constexpr,  # whether beta is headwise vector or scalar,
+    USE_QK_L2NORM_IN_KERNEL: tl.constexpr,
+    IS_VARLEN: tl.constexpr,  # yes, it‘s variable length
+    IS_CONTINUOUS_BATCHING: tl.constexpr,
+    IS_SPEC_DECODING: tl.constexpr,
+):
+    i_k, i_v, i_nh = tl.program_id(0), tl.program_id(1), tl.program_id(2)
+    i_n, i_hv = i_nh // HV, i_nh % HV
+    i_h = i_hv // (HV // H)
+    if IS_VARLEN:
+        bos, eos = (
+            tl.load(cu_seqlens + i_n).to(tl.int64),
+            tl.load(cu_seqlens + i_n + 1).to(tl.int64),
+        )
+        all = T
+        T = eos - bos
+    else:
+        bos, eos = i_n * T, i_n * T + T
+        all = B * T
+
+    if T == 0:
+        # no tokens to process for this sequence
+        return
+
+    o_k = i_k * BK + tl.arange(0, BK)
+    o_v = i_v * BV + tl.arange(0, BV)
+
+    p_q = q + bos * stride_q_t + i_h * stride_q_h + o_k * stride_q_k
+    p_k = k + bos * stride_k_t + i_h * stride_k_h + o_k * stride_k_k
+    p_v = v + bos * stride_v_t + i_hv * stride_v_hv + o_v * stride_v_v
+    if IS_BETA_HEADWISE:
+        p_beta = beta + (bos * HV + i_hv) * V + o_v
+    else:
+        p_beta = beta + bos * HV + i_hv
+
+    p_g = g + bos * HV + i_hv
+
+    p_o = o + ((i_k * all + bos) * HV + i_hv) * V + o_v
+
+    mask_k = o_k < K
+    mask_v = o_v < V
+    mask_h = mask_k[:, None] & mask_v[None, :]
+
+    b_h = tl.zeros([BK, BV], dtype=tl.float32)
+    if USE_INITIAL_STATE:
+        if IS_CONTINUOUS_BATCHING:
+            if IS_SPEC_DECODING:
+                i_t = tl.load(num_accepted_tokens + i_n).to(tl.int64) - 1
+            else:
+                i_t = 0
+            p_h0 = (
+                h0
+                + tl.load(ssm_state_indices + i_n * stride_indices_seq + i_t).to(
+                    tl.int64
+                )
+                * stride_init_state_token
+            )
+        else:
+            p_h0 = h0 + bos * HV * K * V
+        p_h0 = p_h0 + i_hv * K * V + o_k[:, None] * V + o_v[None, :]
+        b_h += tl.load(p_h0, mask=mask_h, other=0).to(tl.float32)
+
+    for i_t in range(0, T):
+        b_q = tl.load(p_q, mask=mask_k, other=0).to(tl.float32)
+        b_k = tl.load(p_k, mask=mask_k, other=0).to(tl.float32)
+        b_v = tl.load(p_v, mask=mask_v, other=0).to(tl.float32)
+
+        if USE_QK_L2NORM_IN_KERNEL:
+            b_q = b_q / tl.sqrt(tl.sum(b_q * b_q) + 1e-6)
+            b_k = b_k / tl.sqrt(tl.sum(b_k * b_k) + 1e-6)
+        b_q = b_q * scale
+        # [BK, BV]
+        b_g = tl.load(p_g).to(tl.float32)
+        b_h *= exp(b_g)
+        # [BV]
+        b_v -= tl.sum(b_h * b_k[:, None], 0)
+        if IS_BETA_HEADWISE:
+            b_beta = tl.load(p_beta, mask=mask_v, other=0).to(tl.float32)
+        else:
+            b_beta = tl.load(p_beta).to(tl.float32)
+        b_v *= b_beta
+        # [BK, BV]
+        b_h += b_k[:, None] * b_v[None, :]
+        # [BV]
+        b_o = tl.sum(b_h * b_q[:, None], 0)
+        tl.store(p_o, b_o.to(p_o.dtype.element_ty), mask=mask_v)
+
+        # keep the states for multi-query tokens
+        if INPLACE_FINAL_STATE:
+            p_ht = (
+                ht
+                + tl.load(ssm_state_indices + i_n * stride_indices_seq + i_t).to(
+                    tl.int64
+                )
+                * stride_final_state_token
+            )
+        else:
+            p_ht = ht + (bos + i_t) * stride_final_state_token
+        p_ht = p_ht + i_hv * K * V + o_k[:, None] * V + o_v[None, :]
+        tl.store(p_ht, b_h.to(p_ht.dtype.element_ty), mask=mask_h)
+
+        # p_q += H * K
+        # p_k += H * K
+        # p_v += HV * V
+        p_q += stride_q_t
+        p_k += stride_k_t
+        p_v += stride_v_t
+        p_o += HV * V
+        p_g += HV
+        p_beta += HV * (V if IS_BETA_HEADWISE else 1)
+
 
 @triton.heuristics(
     {
@@ -356,24 +511,16 @@ def fused_recurrent_gated_delta_rule_fwd(
     B, T, H, K, V = *k.shape, v.shape[-1]
     HV = v.shape[2]
     N = B if cu_seqlens is None else len(cu_seqlens) - 1
-<<<<<<< HEAD
     BK, BV = triton.next_power_of_2(K), min(triton.next_power_of_2(V), 32)
-=======
-    BK, BV = triton.next_power_of_2(K), min(triton.next_power_of_2(V), 8)
->>>>>>> df000e19 (update fla kernels)
     NK, NV = triton.cdiv(K, BK), triton.cdiv(V, BV)
     assert NK == 1, "NK > 1 is not supported yet"
     num_stages = 3
     num_warps = 1
-<<<<<<< HEAD
     qkv_contiguous = (
         (q.stride(0) == q.stride(1) + q.stride(2))
         and (k.stride(0) == k.stride(1) + k.stride(2))
         and (v.stride(0) == v.stride(1) + v.stride(2))
     )
-=======
->>>>>>> df000e19 (update fla kernels)
-
     o = q.new_empty(NK, *v.shape)
     if inplace_final_state:
         final_state = initial_state
@@ -391,7 +538,6 @@ def fused_recurrent_gated_delta_rule_fwd(
         stride_indices_seq, stride_indices_tok = ssm_state_indices.stride()
 
     grid = (NK, NV, N * HV)
-<<<<<<< HEAD
     if qkv_contiguous:
         fused_recurrent_gated_delta_rule_fwd_kernel[grid](
             q=q,
@@ -500,41 +646,5 @@ def fused_recurrent_gated_delta_rule_fwd(
             num_warps=num_warps,
             num_stages=num_stages,
         )
-
-=======
-    fused_recurrent_gated_delta_rule_fwd_kernel[grid](
-        q=q,
-        k=k,
-        v=v,
-        g=g,
-        beta=beta,
-        o=o,
-        h0=initial_state,
-        ht=final_state,
-        cu_seqlens=cu_seqlens,
-        ssm_state_indices=ssm_state_indices,
-        num_accepted_tokens=num_accepted_tokens,
-        scale=scale,
-        N=N,
-        T=T,
-        B=B,
-        H=H,
-        HV=HV,
-        K=K,
-        V=V,
-        BK=BK,
-        BV=BV,
-        stride_init_state_token=stride_init_state_token,
-        stride_final_state_token=stride_final_state_token,
-        stride_indices_seq=stride_indices_seq,
-        stride_indices_tok=stride_indices_tok,
-        IS_BETA_HEADWISE=beta.ndim == v.ndim,
-        USE_QK_L2NORM_IN_KERNEL=use_qk_l2norm_in_kernel,
-        INPLACE_FINAL_STATE=inplace_final_state,
-        IS_KDA=False,
-        num_warps=num_warps,
-        num_stages=num_stages,
-    )
->>>>>>> df000e19 (update fla kernels)
     o = o.squeeze(0)
     return o, final_state
