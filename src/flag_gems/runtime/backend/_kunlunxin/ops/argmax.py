5ac1a16ec203169877f1276f5804a80f915c78bd
DuanYaQi <30496905+DuanYaQi@users.noreply.github.com>
2026-01-19 19:39:05 +0800

    [kunlunxin] fix kunlunxin backend op log (#1461)
    * fix kunlunxin backend op log
    
    * fix elu & mm
    
    * use old log
    
    * use old log

--- src/flag_gems/runtime/backend/_kunlunxin/ops/argmax.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/argmax.py
@@ -11,7 +11,7 @@ from flag_gems.utils import libentry
 from flag_gems.utils import triton_lang_extension as tle
 from flag_gems.utils.limits import get_dtype_min
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger("flag_gems").getChild(__name__.lstrip("."))
 
 
 @libentry()
